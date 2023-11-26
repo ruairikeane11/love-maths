3f86ea5e6ce55226f415cbf5ab090d296cada238
ruairi keane <ruairi.rk@icloud.com>
2023-11-26 19:44:10 +0000

    add display question to server

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,14 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+   runGame("addition");
+
+
 });
 
 /**
@@ -22,11 +27,19 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the users answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
 
     // creates 2 random numbers from 1-25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType ==="addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert (`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!` ;
+    }
+ 
 
 }
 
@@ -46,8 +59,10 @@ function incremenetWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+       document.getElementById('operand1').textContent = operand1;
+       document.getElementById('operand2').textContent = operand2;
+       document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
